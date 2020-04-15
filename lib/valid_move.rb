044ad3eb676860d6bfa140faa58389ab9e55f99d
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-15 14:31:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,24 +1,11 @@
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && within_board(int) == true
-    return true
-  else 
-    return false
-  end
-end
-def position_taken?(board, index)
-if board[index] == " " || board[index] == "" || board[index] == nil 
+  def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
   return false
-else
-  return true
-end
-end
-def within_board(int)
-  if int.between?(0,8) == true
-    return true
   else
-    return false
-  end 
-end
+  return true
+
+
 
  
     
